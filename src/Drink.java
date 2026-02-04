8410577b2bbd25f92223e88f677e43b818b2f3f6
Rosen Ivanov <rrirrirri08@gmail.com>
2026-02-04 20:05:55 +0200

    feat: added new claass for drink

--- src/Drink.java
+++ src/Drink.java
@@ -1,3 +1,10 @@
-public class Drink {
+public class Drink extends BaseDrink {
+    public Drink(DrinkType type) {
+        super(type.getName(), type.getCost(), type.getIngredients());
+    }
 
+    @Override
+    public void prepare() {
+
+    }
 }
