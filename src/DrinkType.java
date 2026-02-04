a3e32435229f17d472f5338254672fe92ee99266
Rosen Ivanov <rrirrirri08@gmail.com>
2026-02-04 19:59:19 +0200

    feat: adding drinks

--- src/DrinkType.java
+++ src/DrinkType.java
@@ -1,3 +1,37 @@
-public enum DrinkType {
+import java.util.Map;
 
+public enum DrinkType {
+    ESPRESSO(
+            "Espresso",
+            0.80,
+            Map.of(
+                    Ingredients.WATER, 250,
+                    Ingredients.MILK, 0,
+                    Ingredients.COFFEE, 24
+            )
+    ),
+
+    LATTE(
+            "Latte",
+            1.50,
+            Map.of(
+                    Ingredients.WATER, 200,
+                    Ingredients.MILK, 150,
+                    Ingredients.COFFEE, 24
+            )
+    ),
+
+    CAPPUCCINO(
+            "Cappuccino",
+            1.40,
+            Map.of(
+                    Ingredients.WATER, 200,
+                    Ingredients.MILK, 100,
+                    Ingredients.COFFEE, 24
+            )
+    );
+
+    private final String name;
+    private final double cost;
+    private final Map<Ingredients, Integer> ingredients;
 }
