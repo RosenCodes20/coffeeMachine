5f7bb655156426952d24cf01aee34b550e91f035
Rosen Ivanov <rrirrirri08@gmail.com>
2026-02-04 20:19:19 +0200

    feat: added more for prepare method

--- src/Drink.java
+++ src/Drink.java
@@ -5,6 +5,10 @@ public class Drink extends BaseDrink {
 
     @Override
     public void prepare() {
-
+        System.out.println("Preparing " + getName() + "...");
+        getIngredients().forEach((ingredient, amount) ->
+                System.out.println(ingredient + ": " + amount)
+        );
+        System.out.println(getName() + " is ready!\n");
     }
 }
